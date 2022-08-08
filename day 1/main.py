c15a91cf26e15db01dd659a7c27a1e17592d416f
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-08-07 20:09:44 -0700

    day one of python completed

---
+++ day 1/main.py
@@ -0,0 +1,26 @@
+# print("Hello World!")
+# print("Hello World!\nHello World!")
+# print("Hello" + " " + "world!")
+# name = input("What's your name?")
+# print("Hello" + " " + input("What is your name?") + "!")
+# print(len(input("What is your name?")))
+# name = "Huynh"
+# name = "Person"
+# length = len(name)
+# tony = 'tony'
+# huynh = 'huynh'
+
+# temp = tony
+# tony = huynh
+# huynh = temp
+# print(tony, huynh)
+
+#1 Create a greeting for your program.
+#2 Ask the user for the city that they grew up in.
+#3 Ask the user for the name of a pet
+#4 Combine the name of their city and pet and show their band  name.
+#5 Make sure the input cursor shows on a new line, see the example at:
+print("Welcome to the Brand Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What is your pet's name?\n")
+print("Your brand name is: " + city + pet)
