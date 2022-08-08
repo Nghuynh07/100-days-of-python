450f88daa52a3cc988fa1d3bbc41e578712fbd11
Nghuynh07 <79157586+Nghuynh07@users.noreply.github.com>
2022-08-07 20:18:29 -0700

    What did I learned on day 1 of python?

--- day 1/main.py
+++ day 1/main.py
@@ -18,9 +18,18 @@
 #1 Create a greeting for your program.
 #2 Ask the user for the city that they grew up in.
 #3 Ask the user for the name of a pet
-#4 Combine the name of their city and pet and show their band  name.
+#4 Combine the name of their city and pet and show their brand  name.
 #5 Make sure the input cursor shows on a new line, see the example at:
 print("Welcome to the Brand Name Generator.")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What is your pet's name?\n")
 print("Your brand name is: " + city + pet)
+
+
+#What did I learn today in python day 1?
+#I learned to print string into console.
+#I learned to skip to the next line or print the second item into the next line  using \n
+#I learned to concatenate string
+#I learned input() amd len() functions
+#I learned variables - setting input/len or name to variables
+#I also did a  mini project that asked for a user's grew they grew in and their pet name and once I get that information, I combined their answer into a single name.
